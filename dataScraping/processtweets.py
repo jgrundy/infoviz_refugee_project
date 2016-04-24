d95afefcda9629c6f3fd752eb6848ff27a75cc4d
Elixeus <eliseowang@gmail.com>
2016-04-24 14:45:07 -0400

    add scraper

---
+++ dataScraping/processtweets.py
@@ -0,0 +1,22 @@
+import csv
+
+
+def getdata(tweet):
+    try:
+        message = tweet.split(",\"text\":\"")[1].split("\",")[0]
+    except:
+        return False
+    try:
+        location = tweet.split("\"location\":\"")[1].split("\",")[0]
+    except:
+        location = " "
+    try:
+        country = tweet.split("\"country\":\"")[1].split("\",")[0]
+    except:
+        country = " "
+    try:
+        time_zone = tweet.split("\"time_zone\":\"")[1].split("\",")[0]
+    except:
+        time_zone = " "
+
+    return message, location, country, time_zone
